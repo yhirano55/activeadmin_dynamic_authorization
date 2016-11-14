36e7de51c8ad2660aac6090b6118b793b0ebec07
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2016-11-14 12:47:45 +0900

    modify ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authorize_access_resource!, except: %i(index show new create edit update destroy google_oauth2)
+
+  def authorize_access_resource!
+    authorize_resource!(active_admin_config.resource_class) if active_admin_config.respond_to?(:resource_class)
+  end
 end
